a28de5e1f99709122f41a6ff61f1947c8b466ac0
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-10-04 00:04:07 +0600

    added skills section

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -3,7 +3,10 @@
 const Skills = () => {
     return (
         <div>
-            <h3>This is skills section</h3>
+            <div>
+             <h3 className="text-center text-3xl font-bold p-3">Skills</h3>
+             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab cupiditate laboriosam eum minima sunt placeat asperiores, accusantium quod id sit.</p>
+            </div>
         </div>
     );
 };
