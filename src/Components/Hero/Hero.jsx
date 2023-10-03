4184c24fb9bd8e6e272961d0bacffe72db32a8f2
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-10-03 23:59:04 +0600

    make hero section responsive

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -4,7 +4,7 @@ import img2 from "../../assets/images/header-img.svg"
 
 const Hero = () => {
     return (
-        <div className="grid grid-cols-2 items-center p-20" style={{ backgroundImage:`url(${img1})` }}>
+        <div className="grid grid-cols-1 lg:grid-cols-2 items-center p-20 bg-cover" style={{ backgroundImage:`url(${img1})` }}>
            <div>
                 <button className="px-6 p-2 text-center text-lg border-2 my-2">Learn More </button>
                 <h3 className="text-4xl font-bold text-pink-500 my-2">Let`s Do with Us</h3>
@@ -12,7 +12,7 @@ const Hero = () => {
                 <a href="/">Let`s Connect</a>
            </div>
            <div>
-                <img src={img2} alt="" />
+                <img className="p-8" src={img2} alt="" />
            </div>
         </div>
     );
