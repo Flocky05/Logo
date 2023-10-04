cb85bb1e9772d20764024e798e5478abd75a172a
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-10-04 18:11:56 +0600

    added project section

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,8 +1,22 @@
-
+import img1 from "../../assets/images/project-img1.png"
+import img2 from "../../assets/images/project-img2.png"
+import img3 from "../../assets/images/project-img3.png"
 const Projects = () => {
     return (
         <div>
-            <p>This is project section</p>
+            <h2 className="text-3xl font-semibold text-center p-6 py-10">This is Project Section</h2>
+            <div className="grid grid-cols-3 gap-5">
+                <div className="p-8  hover:transform hover:skew-y-12 duration-700">
+                    <img src={img1} alt="" />
+                </div>
+                <div className="p-8 hover:p-0 duration-700">
+                    <img src={img2} alt="" />
+                </div>
+                <div className="p-8 hover:transform hover:-skew-y-12 duration-700">
+                    <img src={img3} alt="" />
+                </div>
+                
+            </div>
         </div>
     );
 };
