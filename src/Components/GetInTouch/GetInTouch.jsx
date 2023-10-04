fd95bcf58fc8defcd4d850adfdf77302ed374f7a
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-10-04 22:22:50 +0600

    make responsive  get in section

--- src/Components/GetInTouch/GetInTouch.jsx
+++ src/Components/GetInTouch/GetInTouch.jsx
@@ -2,7 +2,7 @@ import img1 from "../../assets/images/contact-img.svg"
 
 const GetInTouch = () => {
     return (
-        <div className="grid grid-cols-2 items-center p-10 bg-gradient-to-r from-purple-500 to-pink-500">
+        <div className="grid grid-cols-1 lg:grid-cols-2 items-center p-10 bg-gradient-to-r from-purple-500 to-pink-500">
             <div>
                 <img className="p-4" src={img1} alt="" />
             </div>
@@ -14,7 +14,7 @@ const GetInTouch = () => {
                     <input type="text" placeholder="Email" className="border-2 rounded-lg p-3 bg-white" />
                     <input type="text" placeholder="Phone Number" className="border-2 rounded-lg p-3 bg-white" />
                 </div>
-                <textarea rows="10" cols="77" placeholder="Message" className="rounded-lg p-4 bg-white"></textarea>
+                <textarea rows="10" placeholder="Message" className="rounded-lg p-4 bg-white w-full"></textarea>
                 <button className="bg-black text-white px-6 py-2 rounded-lg hover:bg-orange-600 mt-3">Submite</button>
             </div>
         </div>
