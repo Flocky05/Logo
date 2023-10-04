abc3b8a2d5093b79cf60a240ef15628504eff8ae
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-10-04 22:13:22 +0600

    added get in touch section

---
+++ src/Components/GetInTouch/GetInTouch.jsx
@@ -0,0 +1,24 @@
+import img1 from "../../assets/images/contact-img.svg"
+
+const GetInTouch = () => {
+    return (
+        <div className="grid grid-cols-2 items-center p-10 bg-gradient-to-r from-purple-500 to-pink-500">
+            <div>
+                <img className="p-4" src={img1} alt="" />
+            </div>
+            <div>
+                <h2 className="text-4xl font-bold py-6">Get In Touch</h2>
+                <div className="grid grid-cols-2 gap-4 py-4">
+                    <input type="text" placeholder="Frist Name" className="border-2 rounded-lg p-3 bg-white" />
+                    <input type="text" placeholder="Last Name" className="border-2 rounded-lg p-3 bg-white" />
+                    <input type="text" placeholder="Email" className="border-2 rounded-lg p-3 bg-white" />
+                    <input type="text" placeholder="Phone Number" className="border-2 rounded-lg p-3 bg-white" />
+                </div>
+                <textarea rows="10" cols="77" placeholder="Message" className="rounded-lg p-4 bg-white"></textarea>
+                <button className="bg-black text-white px-6 py-2 rounded-lg hover:bg-orange-600 mt-3">Submite</button>
+            </div>
+        </div>
+    );
+};
+
+export default GetInTouch;
