71a302a3afa0f04d41530f63b3c627b4d90cad80
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-10-04 17:47:17 +0600

    update hero section

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
     return (
         <div className="grid grid-cols-1 lg:grid-cols-2 items-center p-20 bg-cover" style={{ backgroundImage:`url(${img1})` }}>
            <div>
-                <button className="px-6 p-2 text-center text-lg border-2 my-2">Learn More </button>
+                <button className="px-6 p-2 text-center text-lg border-2 my-2 hover:text-pink-500">Learn More </button>
                 <h3 className="text-4xl font-bold text-pink-500 my-2">Let`s Do with Us</h3>
                 <p className="my-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae quam molestias, obcaecati sit commodi deserunt vero numquam ducimus! Amet, rem temporibus fugit autem cupiditate accusamus quos unde repellendus, quia libero reprehenderit quas. Ab ullam quas consequuntur porro illo, quia quo?</p>
                 <a href="/">Let`s Connect</a>
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,12 +2,13 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <div className="flex justify-between bg-fuchsia-900 text-2xl p-4 px-10">
-            <Link to="/">Logo</Link>
-            <div className="grid grid-cols-3 gap-10">
-                <Link to="/">Home</Link>
-                <Link to="/skills">Skills</Link>
-                <Link to="/projects">Projects</Link>
+        <div className="flex justify-between bg-violet-950 text-xl p-4 px-10 shadow-lg">
+            <Link className="hover:text-fuchsia-500" to="/">Logo</Link>
+            <div className="grid grid-cols-4 items-center gap-10">
+                <Link className="hover:text-fuchsia-500" to="/">Home</Link>
+                <Link className="hover:text-fuchsia-500" to="/skills">Skills</Link>
+                <Link className="hover:text-fuchsia-500" to="/projects">Projects</Link>
+                <Link className="hover:text-fuchsia-500" to="/"><button className="border-2 px-4 py-1 rounded-lg">More</button></Link>
             </div>
         </div>
     );
